f52aca48ca8abd5218ae11705497f13a55979dae
tonyvu2014 <tonyvu2008@gmail.com>
2015-11-01 21:18:23 +0800

    Change to Saleable

--- src/com/android/tonyvu/sc/model/Saleable.java
+++ src/com/android/tonyvu/sc/model/Saleable.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 /**
  * Implements this interface for any product which can be added to shopping cart
+ * 
+ * The product class must also override {@code equals()} and {@code hashCode()} methods 
  *
  */
 public interface Saleable {
